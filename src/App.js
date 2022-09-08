8dabcff87d6a277f6ed2933782197f14435b4600
HernanGH <hjcondori04@gmail.com>
2022-09-07 22:31:10 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('hola esto esta funcionando');
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola coder!!! Clase 2
         </p>
         <a
           className="App-link"
