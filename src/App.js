0d84103974c0da5ffb41cd72f132c2dcccd61e40
HernanGH <hjcondori04@gmail.com>
2022-10-03 22:40:07 -0300

    clase 9

--- src/App.js
+++ src/App.js
@@ -9,18 +9,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cart from './components/Cart/Cart';
 import Checkout from './components/Checkout';
 import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
+import Events from './components/Events';
+import Intercambiabilidad from './components/Intercambiabilidad';
 
 function App() {
     return (
       <BrowserRouter basename='/reactjs38100'>
         <NavBar />
-        <Routes>
+        {/* <Events /> */}
+        <Intercambiabilidad />
+        {/* <Routes>
           <Route path='/' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
           <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Bienvenidos a mi tienda'} />} />
           <Route path='/item/:id' element={<ItemDetailContainer />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/checkout' element={<Checkout />} />
-        </Routes>
+        </Routes> */}
       </BrowserRouter>
     );
 }
